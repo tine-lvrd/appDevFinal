6f599136cf57c8cc3e0357f77da00bec7cbf9328
unknown <main.annalorraine.peletina@cvsu.edu.ph>
2025-01-12 22:47:28 +0800

    Course Detail Added

---
+++ Components/CourseIntro.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function CourseIntro() {
+  return (
+    <View>
+      <Text>CourseIntro</Text>
+    </View>
+  )
+}
--- Screens/CourseDetailScreen.jsx
+++ Screens/CourseDetailScreen.jsx
@@ -1,10 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, Touchable } from 'react-native'
 import React from 'react'
+import { useRoute } from '@react-navigation/native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function CourseDetailScreen() {
+    const {params}=useRoute();
+    const[course, setCourse]=useState();
+    const navigation=useNavigation();
+    usereffect (()=>{
+        setCourse(params.course)
+    },[params])
   return (
-    <View>
-      <Text>CourseDetailScreen</Text>
+    <View style={{padding: 20, marginTop:25}}>
+      <View style={{display:'flex', flexDirection: 'row',alignItems: 'center', gap:50}}>
+        <TouchableOpacity onPress={()=>navigation.goBack()}>
+        <Ionicons name="arrow-back-circle-sharp" size={40} color="black" />
+        </TouchableOpacity>
+      <Text style={{fontSize:27, fontFamily: 'outfit-bold'}}>Course Detail</Text>
+      </View>
     </View>
   )
 }
