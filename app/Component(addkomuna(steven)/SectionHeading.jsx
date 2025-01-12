960e0f3ba056e3d0566e49562e6496368dd3a7b0
clutch_10 <168260980+1v0clutch@users.noreply.github.com>
2025-01-12 21:52:51 +0800

    added sectionheading.jsx pt2 (di pala naadd sorrie)

---
+++ app/Component(addkomuna(steven)/SectionHeading.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function SectionHeading() {
+  return (
+    <View>
+      <Text style={{fontFamily:'outfit-medium',fontSize:20,marginBottom:5,color:Colors.PRIMARY}}>Category</Text>
+    </View>
+  )
+}
