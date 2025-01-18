2f69f3a3cd90b59fe9bfaa87f9ad42e560939ad7
unknown <trixie08vargas@gmail.com>
2025-01-18 18:37:53 +0800

    sa index, sa homescreen, sa courselist

--- Screens/HomeScreen.jsx
+++ Screens/HomeScreen.jsx
@@ -1,19 +1,58 @@
-import { View, Text } from 'react-native'
-
+import { View, Text, Button } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
 import GlobalApi from "../Utils/GlobalApi"
+import {client} from '../Utils/KindConfig';
+import SectionHeading from './SectionHeading'
 
-//error hindi pa tapos need pa ayusin
-const getCourseList=()=> {
-    GlobalApi.getCourseList().then(resp=>{})
-        console.log(resp)
+export default function HomeScreen() {
+    const {auth,setAuth}=useContext(AuthContext)
+    const [categories,setCategories]=useState();
+    useEffect(()=>{
+        getCategory();
+    },[])
+    const loggedOut = await.client.logout();
+    if (loggedOut) {
+        setAuth(false);
+            // User was logged out
+    };
+
+    //Get Category List
+    const getCategory=() =>{
+        const getCategory=()=>{
+            GlobalApi.getCategory().then(resp=>{
+                setCategories(resp.categories);
+            })
+        }
+    }
+};
+    return (
+        <View>
+            <Text>HomeScreen</Text>
+            <Button title='Logout' onPress={handleLogout} />
+        </View>
+    )
+
+const getCourseList = () => {
+    GlobalApi.getCourseList().then(resp => { 
+    setCourseList(resp?.CourseList)
+    })
 }
+const getFilterCourseList=(tag)=>{
+    const result=courseList.filter((item)=>item.tag.includes(tag));
+    return result
+}
+    return(
+        <ScrollView style={{ padding: 20, marginTop: 25 }}>
+            <Header/>
+            {/* Category List */}
+             <CategoryList categories={categories} />
 
+             {/* All Course List */}
+            <SectionHeading heading={'Latest Courses'} />
+            <CourseList courseList={courseList} />
 
-
-<View style={{padding:20,marginTop:25}}>
-    {/*Course List */}
-    <SectionHeading heading={'Latest Courses'} />
-    <CourseList/>
-
-
-</View>
+            {/* React Native Course List */}
+            <SectionHeading heading={'React Native Courses'} />
+            <CourseList courseList={getFilterCourseList('react_native')} />
+        </ScrollView>
+    )
--- app/Components/CourseList.jsx
+++ app/Components/CourseList.jsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
+import Item from './CourseItem'
+import CourseItem from './CourseItem'
 
-export default function CourseList () {
+export default function CourseList({ courseList }) {
     return (
         <View>
-            <Text>CourseList</Text>
+            <FlatList
+                data={courseList}
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                renderItem={({ item, index }) => (
+                    <CourseItem course={item} />
+                )} />
         </View>
-    )
+    );
 }
